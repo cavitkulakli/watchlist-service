2f5473a453edf5303722c72febad0923ca43fafc
kulakli_cavit@hotmail.com <kulakli_cavit@hotmail.com>
2020-01-07 23:36:41 +0300

    added gatewayapp for filtering and multiproject gradle structure

---
+++ gatewayapplication/src/main/java/GatewayApplication.java
@@ -0,0 +1,20 @@
+import filters.DummyFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public DummyFilter simpleFilter() {
+        return new DummyFilter();
+    }
+
+}
